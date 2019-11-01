4366aac40abda647b58f588c3d3034624f9b0830
blewis095 <57243497+blewis095@users.noreply.github.com>
2019-10-31 22:55:09 -0400

    Add files via upload

---
+++ designs.js
@@ -0,0 +1,37 @@
+// Select color input
+// Select size input
+const $tableElement = $('#pixelCanvas');
+const $inputHeight = $('#inputHeight');
+const $inputWidth = $('#inputWidth');
+const $colorPicker = $('#colorPicker');
+
+$('#sizePicker').submit( event => {
+    event.preventDefault();
+
+    let width = $inputWidth.val();
+    let height = $inputHeight.val();
+
+    $tableElement.html(''); //clear
+
+    makeGrid(height, width);
+    addCellClickListener();
+});
+
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid(height, width) {
+    for(let i = 0; i < height; i++) {
+        $tableElement.append('<tr></tr>');
+    };
+
+    for(let i = 0; i < width; i++) {
+        $('tr').append('<td></td>');
+    };
+};
+
+function addCellClickListener() {
+    $('td').click( event => {
+        let color = $colorPicker.val();
+        $(event.currentTarget).css("background-color", color)
+    });
+};
